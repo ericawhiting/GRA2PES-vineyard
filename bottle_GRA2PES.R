d6bfebc520cf7d2381b0c7d0dce50824bb0e3df6
Erica Whiting <erwh@umich.edu>
2025-08-13 17:08:22 -0400

    add comments and function descriptions

--- bottle_GRA2PES.R
+++ bottle_GRA2PES.R
@@ -1,3 +1,6 @@
+# part of GRA2PES-VINEYARD to simplify GRA2PES files to read surface CO and aggregate by month
+# Erica Whiting (erwh@umich.edu)
+
 # -------------------------
 # LOAD LIBRARIES
 # -------------------------
@@ -24,147 +27,148 @@ year_month_list <- read_txt_list("GRA2PES_months.txt")
 # DEFINE FUNCTION TO READ FILES PER YEAR MONTH
 # -------------------------
 create_monthly_file_from_filtered_GRA2PES <- function(year_month) {
-  #' take in year and month to open GRA2PES files that have one gas at surface level and aggregate
-  #' @param year_month string from year_month_list YYYYMM
-  #' aggregates GRA2PES weekday, saturday, and sunday 00-11 and 12-23 hour files into month
-  #' no return, but saves out a nc file for year month of weighted day of week data for month
-  # -------------------------
-  GRA2PES_CO_path <- "/no_backup/erwh/GRA2PES/CO/"
-  # -------------------------
-  # weekday files
-  wk_00_11 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_weekdy_00to11Z_weekdy_COsurface.nc"))
-  wk_12_23 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_weekdy_12to23Z_weekdy_COsurface.nc"))
-  # saturday files
-  sa_00_11 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_satdy_00to11Z_satdy_COsurface.nc"))
-  sa_12_23 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_satdy_12to23Z_satdy_COsurface.nc"))
-  # sunday files
-  su_00_11 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_sundy_00to11Z_sundy_COsurface.nc"))
-  su_12_23 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_sundy_12to23Z_sundy_COsurface.nc"))
-  # -------------------------
-  # var: "XLAT"  "XLONG" "Times" "CO"
-  wk_00_11_xlat <- ncvar_get(wk_00_11, "XLAT")
-  wk_12_23_xlat <- ncvar_get(wk_12_23, "XLAT")
-  sa_00_11_xlat <- ncvar_get(sa_00_11, "XLAT")
-  sa_12_23_xlat <- ncvar_get(sa_12_23, "XLAT")
-  su_00_11_xlat <- ncvar_get(su_00_11, "XLAT")
-  su_12_23_xlat <- ncvar_get(su_12_23, "XLAT")
+    #' take in year and month to open GRA2PES files that have one gas at surface level and aggregate
+    #' @param year_month string from year_month_list YYYYMM
+    #' aggregates GRA2PES weekday, saturday, and sunday 00-11 and 12-23 hour files into month
+    #' no return, but saves out a nc file for year month of weighted day of week data for month
+    #' CO surface data saved as mole km^-2 hr^-1 for 4km x 4km cells
+    # -------------------------
+    GRA2PES_CO_path <- "/no_backup/erwh/GRA2PES/CO/"
+    # -------------------------
+    # weekday files
+    wk_00_11 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_weekdy_00to11Z_weekdy_COsurface.nc"))
+    wk_12_23 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_weekdy_12to23Z_weekdy_COsurface.nc"))
+    # saturday files
+    sa_00_11 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_satdy_00to11Z_satdy_COsurface.nc"))
+    sa_12_23 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_satdy_12to23Z_satdy_COsurface.nc"))
+    # sunday files
+    su_00_11 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_sundy_00to11Z_sundy_COsurface.nc"))
+    su_12_23 <- nc_open(paste0(GRA2PES_CO_path, "GRA2PESv1.0_total_", year_month, "_sundy_12to23Z_sundy_COsurface.nc"))
+    # -------------------------
+    # var: "XLAT"  "XLONG" "Times" "CO"
+    wk_00_11_xlat <- ncvar_get(wk_00_11, "XLAT")
+    wk_12_23_xlat <- ncvar_get(wk_12_23, "XLAT")
+    sa_00_11_xlat <- ncvar_get(sa_00_11, "XLAT")
+    sa_12_23_xlat <- ncvar_get(sa_12_23, "XLAT")
+    su_00_11_xlat <- ncvar_get(su_00_11, "XLAT")
+    su_12_23_xlat <- ncvar_get(su_12_23, "XLAT")
 
-  wk_00_11_xlong <- ncvar_get(wk_00_11, "XLONG")
-  wk_12_23_xlong <- ncvar_get(wk_12_23, "XLONG")
-  sa_00_11_xlong <- ncvar_get(sa_00_11, "XLONG")
-  sa_12_23_xlong <- ncvar_get(sa_12_23, "XLONG")
-  su_00_11_xlong <- ncvar_get(su_00_11, "XLONG")
-  su_12_23_xlong <- ncvar_get(su_12_23, "XLONG")
+    wk_00_11_xlong <- ncvar_get(wk_00_11, "XLONG")
+    wk_12_23_xlong <- ncvar_get(wk_12_23, "XLONG")
+    sa_00_11_xlong <- ncvar_get(sa_00_11, "XLONG")
+    sa_12_23_xlong <- ncvar_get(sa_12_23, "XLONG")
+    su_00_11_xlong <- ncvar_get(su_00_11, "XLONG")
+    su_12_23_xlong <- ncvar_get(su_12_23, "XLONG")
 
-  wk_00_11_Time <- ncvar_get(wk_00_11, "Time")
-  wk_12_23_Time <- ncvar_get(wk_12_23, "Time")
-  sa_00_11_Time <- ncvar_get(sa_00_11, "Time")
-  sa_12_23_Time <- ncvar_get(sa_12_23, "Time")
-  su_00_11_Time <- ncvar_get(su_00_11, "Time")
-  su_12_23_Time <- ncvar_get(su_12_23, "Time")
+    wk_00_11_Time <- ncvar_get(wk_00_11, "Time")
+    wk_12_23_Time <- ncvar_get(wk_12_23, "Time")
+    sa_00_11_Time <- ncvar_get(sa_00_11, "Time")
+    sa_12_23_Time <- ncvar_get(sa_12_23, "Time")
+    su_00_11_Time <- ncvar_get(su_00_11, "Time")
+    su_12_23_Time <- ncvar_get(su_12_23, "Time")
 
-  wk_00_11_CO <- ncvar_get(wk_00_11, "CO")
-  wk_12_23_CO <- ncvar_get(wk_12_23, "CO")
-  sa_00_11_CO <- ncvar_get(sa_00_11, "CO")
-  sa_12_23_CO <- ncvar_get(sa_12_23, "CO")
-  su_00_11_CO <- ncvar_get(su_00_11, "CO")
-  su_12_23_CO <- ncvar_get(su_12_23, "CO")
+    wk_00_11_CO <- ncvar_get(wk_00_11, "CO")
+    wk_12_23_CO <- ncvar_get(wk_12_23, "CO")
+    sa_00_11_CO <- ncvar_get(sa_00_11, "CO")
+    sa_12_23_CO <- ncvar_get(sa_12_23, "CO")
+    su_00_11_CO <- ncvar_get(su_00_11, "CO")
+    su_12_23_CO <- ncvar_get(su_12_23, "CO")
 
-  # save units
-  var_units <- ncatt_get(wk_00_11, "CO", "units")$value
-  xlat_units <- ncatt_get(wk_00_11, "XLAT", "units")$value
-  xlong_units <- ncatt_get(wk_00_11, "XLONG", "units")$value
-  # -------------------------
-  # close files
-  nc_close(wk_00_11)
-  nc_close(wk_12_23)
-  nc_close(sa_00_11)
-  nc_close(sa_12_23)
-  nc_close(su_00_11)
-  nc_close(su_12_23)
-  # -------------------------
-  # check that spatial dimensions match
-  latlon_dims <- list(dim(wk_00_11_xlat), dim(wk_12_23_xlat), dim(sa_00_11_xlat), dim(sa_12_23_xlat), dim(su_00_11_xlat), dim(su_12_23_xlat),
-                      dim(wk_00_11_xlong), dim(wk_12_23_xlong), dim(sa_00_11_xlong), dim(sa_12_23_xlong), dim(su_00_11_xlong), dim(su_12_23_xlong))
+    # save units
+    var_units <- ncatt_get(wk_00_11, "CO", "units")$value
+    xlat_units <- ncatt_get(wk_00_11, "XLAT", "units")$value
+    xlong_units <- ncatt_get(wk_00_11, "XLONG", "units")$value
+    # -------------------------
+    # close files
+    nc_close(wk_00_11)
+    nc_close(wk_12_23)
+    nc_close(sa_00_11)
+    nc_close(sa_12_23)
+    nc_close(su_00_11)
+    nc_close(su_12_23)
+    # -------------------------
+    # check that spatial dimensions match
+    latlon_dims <- list(dim(wk_00_11_xlat), dim(wk_12_23_xlat), dim(sa_00_11_xlat), dim(sa_12_23_xlat), dim(su_00_11_xlat), dim(su_12_23_xlat),
+                        dim(wk_00_11_xlong), dim(wk_12_23_xlong), dim(sa_00_11_xlong), dim(sa_12_23_xlong), dim(su_00_11_xlong), dim(su_12_23_xlong))
 
-  time_dims <- list(dim(wk_00_11_Time), dim(wk_12_23_Time), dim(sa_00_11_Time), dim(sa_12_23_Time), dim(su_00_11_Time), dim(su_12_23_Time))
+    time_dims <- list(dim(wk_00_11_Time), dim(wk_12_23_Time), dim(sa_00_11_Time), dim(sa_12_23_Time), dim(su_00_11_Time), dim(su_12_23_Time))
 
-  var_dims <- list(dim(wk_00_11_CO), dim(wk_12_23_CO), dim(sa_00_11_CO), dim(sa_12_23_CO), dim(su_00_11_CO), dim(su_12_23_CO))
+    var_dims <- list(dim(wk_00_11_CO), dim(wk_12_23_CO), dim(sa_00_11_CO), dim(sa_12_23_CO), dim(su_00_11_CO), dim(su_12_23_CO))
 
-  stopifnot(all(sapply(latlon_dims, function(x) identical(x, latlon_dims[[1]]))))
-  stopifnot(all(sapply(time_dims, function(x) identical(x, time_dims[[1]]))))
-  stopifnot(all(sapply(var_dims, function(x) identical(x, var_dims[[1]]))))
+    stopifnot(all(sapply(latlon_dims, function(x) identical(x, latlon_dims[[1]]))))
+    stopifnot(all(sapply(time_dims, function(x) identical(x, time_dims[[1]]))))
+    stopifnot(all(sapply(var_dims, function(x) identical(x, var_dims[[1]]))))
 
-  # -------------------------
-  # Stack by day of week:
-  wk_00_23_CO <- abind(wk_00_11_CO, wk_12_23_CO, along = 3)
-  sa_00_23_CO <- abind(sa_00_11_CO, sa_12_23_CO, along = 3)
-  su_00_23_CO <- abind(su_00_11_CO, su_12_23_CO, along = 3)
-  # -------------------------
-  # create calendar of days of week for year_month
-  year <- as.numeric(str_sub(year_month, 1, 4))
-  month <- as.numeric(str_sub(year_month, 5, 6))
-  dates <- seq(as.Date(sprintf("%04d-%02d-01", year, month)),
-            as.Date(sprintf("%04d-%02d-01", year, month)) + 31, by = "day")
-  dates <- dates[format(dates, "%m") == sprintf("%02d", month)]
-  daytypes <- ifelse(weekdays(dates) == "Saturday", "sat",
-                      ifelse(weekdays(dates) == "Sunday", "sun",
-                            "wk"))
-  # -------------------------
-  # arrange nc file in calendar order
-  day_arrays <- list(wk = wk_00_23_CO, sat = sa_00_23_CO, sun = su_00_23_CO)
-  month_list <- lapply(daytypes, function(dt) day_arrays[[dt]])
+    # -------------------------
+    # Stack by day of week:
+    wk_00_23_CO <- abind(wk_00_11_CO, wk_12_23_CO, along = 3)
+    sa_00_23_CO <- abind(sa_00_11_CO, sa_12_23_CO, along = 3)
+    su_00_23_CO <- abind(su_00_11_CO, su_12_23_CO, along = 3)
+    # -------------------------
+    # create calendar of days of week for year_month
+    year <- as.numeric(str_sub(year_month, 1, 4))
+    month <- as.numeric(str_sub(year_month, 5, 6))
+    dates <- seq(as.Date(sprintf("%04d-%02d-01", year, month)),
+              as.Date(sprintf("%04d-%02d-01", year, month)) + 31, by = "day")
+    dates <- dates[format(dates, "%m") == sprintf("%02d", month)]
+    daytypes <- ifelse(weekdays(dates) == "Saturday", "sat",
+                        ifelse(weekdays(dates) == "Sunday", "sun",
+                              "wk"))
+    # -------------------------
+    # arrange nc file in calendar order
+    day_arrays <- list(wk = wk_00_23_CO, sat = sa_00_23_CO, sun = su_00_23_CO)
+    month_list <- lapply(daytypes, function(dt) day_arrays[[dt]])
 
-  # stack in calendar order
-  month_array <- abind(month_list, along = 4) # new dim: day
+    # stack in calendar order
+    month_array <- abind(month_list, along = 4) # new dim: day
 
-  # collapse hour and day into just hour of month
-  dim(month_array) <- c(dim(month_array)[1:2], 24 * length(dates)) # [lon, lat, 24*n_days]
-  month_array_mean <- apply(month_array, c(1, 2), mean, na.rm = TRUE)
+    # collapse hour and day into just hour of month
+    dim(month_array) <- c(dim(month_array)[1:2], 24 * length(dates)) # [lon, lat, 24*n_days]
+    month_array_mean <- apply(month_array, c(1, 2), mean, na.rm = TRUE)
 
-  # -------------------------
-  # Write to NetCDF
-  # -------------------------
-  # Find x, y, time dimensions
-  nx <- dim(month_array)[1]
-  ny <- dim(month_array)[2]
+    # -------------------------
+    # Write to NetCDF
+    # -------------------------
+    # Find x, y, time dimensions
+    nx <- dim(month_array)[1]
+    ny <- dim(month_array)[2]
 
-  # Define file dimensions
-  west_east_dim <- ncdim_def("west_east", "index", 1:nx)
-  south_north_dim <- ncdim_def("south_north", "index", 1:ny)
+    # Define file dimensions
+    west_east_dim <- ncdim_def("west_east", "index", 1:nx)
+    south_north_dim <- ncdim_def("south_north", "index", 1:ny)
 
-  # Define vars
-  XLAT_var <- ncvar_def("XLAT",  units = xlat_units, dim = list(west_east_dim, south_north_dim), missval = NA, prec = "float")
-  XLONG_var <- ncvar_def("XLONG", units = xlong_units,  dim = list(west_east_dim, south_north_dim), missval = NA, prec = "float")
-  CO_var <- ncvar_def("CO", units = var_units, dim = list(west_east_dim, south_north_dim), missval = NA, prec = "float")
+    # Define vars
+    XLAT_var <- ncvar_def("XLAT",  units = xlat_units, dim = list(west_east_dim, south_north_dim), missval = NA, prec = "float")
+    XLONG_var <- ncvar_def("XLONG", units = xlong_units,  dim = list(west_east_dim, south_north_dim), missval = NA, prec = "float")
+    CO_var <- ncvar_def("CO", units = var_units, dim = list(west_east_dim, south_north_dim), missval = NA, prec = "float")
 
-  # Create dir if doesn't exist
-  monthly_path <- paste0(GRA2PES_CO_path, "monthly")
-  if (!dir.exists(monthly_path)) {
-    dir.create(monthly_path)
-  }
-  # Create file
-  nc_monthly <- nc_create(paste0(monthly_path, "/GRA2PESv1.0_total_", year_month, "_COsurface.nc"),
-                          list(XLAT_var, XLONG_var, CO_var))
+    # Create dir if doesn't exist
+    monthly_path <- paste0(GRA2PES_CO_path, "monthly")
+    if (!dir.exists(monthly_path)) {
+      dir.create(monthly_path)
+    }
+    # create file
+    nc_monthly <- nc_create(paste0(monthly_path, "/GRA2PESv1.0_total_", year_month, "_COsurface.nc"),
+                            list(XLAT_var, XLONG_var, CO_var))
 
-  # add in data to nc file
-  ncvar_put(nc_monthly, XLAT_var, wk_00_11_xlat)
-  ncvar_put(nc_monthly, XLONG_var, wk_00_11_xlong)
-  ncvar_put(nc_monthly, CO_var, month_array_mean)
+    # add in data to nc file
+    ncvar_put(nc_monthly, XLAT_var, wk_00_11_xlat)
+    ncvar_put(nc_monthly, XLONG_var, wk_00_11_xlong)
+    ncvar_put(nc_monthly, CO_var, month_array_mean)
 
-  # add attributes to nc file
-  ncatt_put(nc_monthly, "CO", "_FillValue", NaN)
-  ncatt_put(nc_monthly, "XLAT", "_FillValue", NaN)
-  ncatt_put(nc_monthly, "XLONG", "_FillValue", NaN)
-  ncatt_put(nc_monthly, "CO", "units", var_units)
-  ncatt_put(nc_monthly, "XLAT", "units", xlat_units)
-  ncatt_put(nc_monthly, "XLONG", "units", xlong_units)
+    # add attributes to nc file
+    ncatt_put(nc_monthly, "CO", "_FillValue", NaN)
+    ncatt_put(nc_monthly, "XLAT", "_FillValue", NaN)
+    ncatt_put(nc_monthly, "XLONG", "_FillValue", NaN)
+    ncatt_put(nc_monthly, "CO", "units", var_units)
+    ncatt_put(nc_monthly, "XLAT", "units", xlat_units)
+    ncatt_put(nc_monthly, "XLONG", "units", xlong_units)
 
-  # close nc files
-  nc_close(nc_monthly)
+    # close nc file
+    nc_close(nc_monthly)
 
-  # print status
-  print(paste0("GRA2PES bottled for ", year_month))
+    # print status
+    print(paste0("GRA2PES bottled for ", year_month))
 }
 
 # define parallelization
--- filter_GRA2PES.R
+++ filter_GRA2PES.R
@@ -1,16 +1,24 @@
+# file called in harvest_GRA2PES.sh
+# part of GRA2PES-VINEYARD to simplify GRA2PES files to read surface CO and aggregate by month
+# Erica Whiting (erwh@umich.edu)
+
 # -------------------------
-# Load Libraries
+# LOAD LIBRARIES
+# -------------------------
 suppressPackageStartupMessages({
     library(ncdf4)
     library(stringr)
 })
+
 # -------------------------
 # DATA FORMAT:
+# -------------------------
 # each month, sector file has with sub directories: satdy, sundy, weekdy
 # each sub directory has two files, 00-11 and 12-23 HH
 
 # -------------------------
- # LOAD IN ARGS
+# LOAD IN ARGS
+# -------------------------
 args <- commandArgs(trailingOnly = TRUE)
 GRA2PES_sector <- args[1] # nolint: object_name_linter.
 GRA2PES_year_month <- args[2] # nolint: object_name_linter.
@@ -18,29 +26,41 @@ GRA2PES_path <- args [3] # nolint: object_name_linter.
 
 # -------------------------
 # Get GRA2PES DATA REPOSITORY
+# -------------------------
 GRA2PES_directory <- paste0(GRA2PES_path, "/", GRA2PES_year_month) # nolint: object_name_linter.
 
 # -------------------------
 # OPEN GRA2PES FILE AND SAVE SURFACE CO IN NEW .NC FILE
+# -------------------------
 open_and_pull_surface_co <- function(file_name, GRA2PES_path) {
+    #' open and save out surface-level CO emissions data from GRA2PES files
+    #' @param file_name
+    #' @param GRA2PES_path path to where GRA2PES files can be accessed on device and where CO dir will be created for output
+    #' no return but writes nc file for each file (weekdy, satdy, sundy; 00to11Z, 12to23Z)
+    #' output nc files are saved in /CO/ dir following GRA2PES_path param
+    #' CO surface data saved as mole km^-2 hr^-1 for 4km x 4km cells
+
     nc <- nc_open(file_name)
-    # Dimensions
+    # read dimensions
     west_east <- nc$dim$west_east$len
     south_north <- nc$dim$south_north$len
     Time_len <- nc$dim$Time$len
 
-    # Variables
+    # read variables
     XLAT <- ncvar_get(nc, "XLAT")    # dim: west_east, south_north
     XLONG <- ncvar_get(nc, "XLONG")  # dim: west_east, south_north
     Times <- ncvar_get(nc, "Times")  # dim: character, Time
     Time <- ncvar_get(nc, "Time")    # dim: Time
 
-    # CO at BOTTOM (surface) level: dim: west_east, south_north, 1, Time
+    # pull CO at BOTTOM (surface) level: dim: west_east, south_north, 1, Time
     CO_surf <- ncvar_get(nc, "CO", start = c(1, 1, 1, 1), count = c(west_east, south_north, 1, Time_len))
 
     # Close GRA2PES nc file
     nc_close(nc)
 
+    # -------------------------
+    # Write surface CO to NetCDF
+    # -------------------------
     # Define dimensions
     dim_west_east <- ncdim_def("west_east", units = "index", vals = 1:west_east)
     dim_south_north <- ncdim_def("south_north", units = "index", vals = 1:south_north)
@@ -52,20 +72,20 @@ open_and_pull_surface_co <- function(file_name, GRA2PES_path) {
     var_times <- ncvar_def("Times", "", list(dim_Time), prec = "char", longname = "char_times")
     var_co <- ncvar_def("CO", "mole km^-2 hr^-1", list(dim_west_east, dim_south_north, dim_Time), missval = NA, prec = "float")
 
-    # Create new file and write variables
+    # create file name and path, store in new directory called CO
     original_name <- str_split(file_name, "/")[[1]][7]
-
-    print(paste0(GRA2PES_path, "/CO/"))
-    # store in new directory called CO
     newfile <- paste0(GRA2PES_path, "/CO/", str_split(original_name, "Z")[[1]][1], "Z_COsurface.nc")
 
+    # create file
     ncnew <- nc_create(newfile, list(var_xlat, var_xlong, var_times, var_co))
 
+    # add in data to nc file
     ncvar_put(ncnew, var_xlat, XLAT)
     ncvar_put(ncnew, var_xlong, XLONG)
     ncvar_put(ncnew, var_times, Times)
     ncvar_put(ncnew, var_co, CO_surf)
 
+    # close nc files
     nc_close(ncnew)
 }
 
