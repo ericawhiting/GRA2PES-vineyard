dc113fbb27f89871efee9a67eae5e3aae0948652
Erica Whiting <erwh@umich.edu>
2026-07-15 17:39:07 -0400

    use nco commands instead of filter_GRA2PES

--- harvest_GRA2PES.sh
+++ harvest_GRA2PES.sh
@@ -13,7 +13,7 @@ while read -r SECTOR; do
         echo "Harvesting GRA2PES from $YEAR_MONTH, $SECTOR"
         
         # assign url to download files
-        YEAR="${text:0:4}"
+        YEAR="${YEAR_MONTH:0:4}"
         # now downloaded through noaa csl rather than nist.gov
         # GRA2PES_file_url="https://data.nist.gov/od/ds/mds2-3520/GRA2PESv1.0_${SECTOR}_${YEAR_MONTH}.tar.gz"
         GRA2PES_file_url="https://csl.noaa.gov/groups/csl4/gra2pes/datasets/data_v1.1/${YEAR}/GRA2PESv1.1_${SECTOR}_${YEAR_MONTH}.tar.gz"
@@ -36,14 +36,46 @@ while read -r SECTOR; do
        
         echo "Cleaning Complete"
         
+        # -------------------------
         # Process GRA2PES
+        # -------------------------
+        outdir="${GRA2PES_dir}/CO"
 
-        Rscript filter_GRA2PES.R "$SECTOR" "$YEAR_MONTH" "$GRA2PES_dir" || { echo "R script failed for $SECTOR $YEAR_MONTH"; exit 1; }
+        # iterate over day types
+        day_of_week_list=("weekdy" "satdy" "sundy")
+
+        for day_of_week in "${day_of_week_list[@]}"; do
+
+            indir="${GRA2PES_dir}/${YEAR_MONTH}/${day_of_week}"
+
+            # loop over NetCDF files
+            for file in "$indir"/*.nc; do
+                
+                original_name="$(basename "$file")"
+                
+                prefix="${original_name%%Z*}"
+
+                newfile="${outdir}/${prefix}Z_COsurface.nc"
+
+                # trim to lowe
+                ncks -O \
+                    -v CO,lat,lon,time \
+                    -d level,0,0 \
+                    "$file" \
+                    "$newfile"
+            done # closes iterating over time files
+
+        done # closes day of week loops
+
+        # Rscript filter_GRA2PES.R "$SECTOR" "$YEAR_MONTH" "$GRA2PES_dir" || { echo "R script failed for $SECTOR $YEAR_MONTH"; exit 1; }
         echo "Filtering Complete"
+        
+        # -------------------------
         
         # Remove GRA2PES YEAR_MONTH Directory now that variable is saved out
         if [[ -n "$GRA2PES_dir" && -n "$YEAR_MONTH" ]]; then
             rm -r "${GRA2PES_dir}/${YEAR_MONTH}"
+            echo "hi"
         else
             echo "There aren't any grapes over here! Not deleting."
         fi
